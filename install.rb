177b61167f2b175bb45d69e97cd1205fd711da5c
Mars0J <70023143+Mars0J@users.noreply.github.com>
2021-02-11 18:06:36 +0300

    Update and rename install.py to install.rb

---
+++ install.rb
@@ -0,0 +1,12 @@
+
+
+#Modül Kurulumları
+system("clear && pip3 install colorama")
+system("clear && pip3 install carrier")
+system("clear && pip3 install timezone")                           
+system("clear && pip3 install termcolor")
+system("clear && pip3 install phonenumbers")                    
+system("clear && pip3 install colored")
+system("clear && pip3 install geocoder")
+system("clear && pip3 install init")
+
